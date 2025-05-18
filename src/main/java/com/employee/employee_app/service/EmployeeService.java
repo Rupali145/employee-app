d389c1a6637ca39b9c13281bd68fe64f02a0d861
rupali145 <drupa981@gmail.com>
2025-05-18 16:08:22 +0530

    added employee controller

---
+++ src/main/java/com/employee/employee_app/service/EmployeeService.java
@@ -0,0 +1,41 @@
+package com.employee.employee_app.service;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+import com.employee.employee_app.model.Employee;
+
+@Service
+public class EmployeeService {
+
+	private final Map<Long, Employee> employeeMap = new HashMap<>();
+    private Long idCounter = 1L;
+
+    public Employee addEmployee(Employee employee) {
+        employee.setId(idCounter++);
+        employeeMap.put(employee.getId(), employee);
+        return employee;
+    }
+
+    public List<Employee> getAllEmployees() {
+        return new ArrayList<>(employeeMap.values());
+    }
+
+    public Employee updateEmployee(Long id, Employee updated) {
+        Employee existing = employeeMap.get(id);
+        if (existing == null) return null;
+
+        existing.setName(updated.getName());
+        existing.setPosition(updated.getPosition());
+        return existing;
+    }
+
+    public boolean deleteEmployee(Long id) {
+        return employeeMap.remove(id) != null;
+    }
+}
